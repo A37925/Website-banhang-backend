1fc44dfccb38d96dfe3f7ea5f98d608d3c3d478a
Quoc Huy <nguyen7quoc4huy02@gmail.com>
2024-05-18 17:03:14 +0700

    add models and connect db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config()
 
 const app = express();
@@ -8,6 +9,13 @@ const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+mongoose.connect(`mongodb+srv://nguyen7quoc4huy02:${process.env.MONGO_DB}@cluster0.tig9y7r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => {
+    console.log('connect Mongoose success')
+  })
+  .catch((err) => {
+    console.log('connect Mongoose error')
+  })
 
 app.listen(port, () => {
   console.log('sever is running in port: ', + port)
