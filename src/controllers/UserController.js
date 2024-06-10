17be7eb7353d68f79a2698b3dc0d92733be23b4b
Quoc Huy <nguyen7quoc4huy02@gmail.com>
2024-06-10 09:13:42 +0700

    set up mvc

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,6 +3,14 @@ const UserSevice = require('../services/UserSevices')
 const createUser = async (req, res) => {
   try {
       console.log(req.body)
+      const { name, email, password, confirmPassword, phone} = req.body
+      
+      if( !name || !email || !password || !confirmPassword || !phone){
+        return res.status(200).json({
+          status: 'ERR',
+          message: 'The input is required'
+        })
+      }
       const res = await UserSevice.createUser()
       return res.status(200).json(res)
   } catch(e){
--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema(
     image: { type: String, required: true},
     type: { type: String, required: true},
     price: { type: Number, required: true},
-    countInStock: { type: Number, required: true},
+    coutInStock: { type: Number, required: true},
     rating: { type: Number, required: true},
     description: { type: String, required: true},
   },
