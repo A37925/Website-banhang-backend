acb02568a8f19574c54e03228af68f6982346965
Quoc Huy <nguyen7quoc4huy02@gmail.com>
2024-06-16 22:34:17 +0700

    Create API dang ky user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,15 +4,27 @@ const createUser = async (req, res) => {
   try {
       console.log(req.body)
       const { name, email, password, confirmPassword, phone} = req.body
-      
-      if( !name || !email || !password || !confirmPassword || !phone){
+      const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+      const isCheckEmail = reg.test(email)
+      if ( !name || !email || !password || !confirmPassword || !phone){
         return res.status(200).json({
           status: 'ERR',
           message: 'The input is required'
         })
+      } else if(!isCheckEmail){
+          return res.status(200).json({
+            status: 'ERR',
+            message: 'The input is email'
+          })
+      } else if(password !== confirmPassword){
+          return res.status(200).json({
+            status: 'ERR',
+            message: 'The password is equal confirmPassword'
+          })
       }
-      const res = await UserSevice.createUser()
-      return res.status(200).json(res)
+      console.log('isCheckEmail', isCheckEmail)
+      const response = await UserSevice.createUser(req.body)
+      return res.status(200).json(response)
   } catch(e){
       return res.status(404).json({
         message: e
