580c50d69fb7d6cae82db0d9f67265092ba479dd
Quoc Huy <nguyen7quoc4huy02@gmail.com>
2024-07-22 00:33:27 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 dotenv.config()
@@ -8,6 +9,7 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors())
 app.use(bodyParser.json())
 
 routes(app);
